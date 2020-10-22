7043bc4f0492bb4cb5ee4bf614b66a2c913cec6c
smashflashtech <smash.flash.tech@gmail.com>
2020-10-22 12:33:23 -0700

    set up user model and set up sign up post rout

---
+++ models/user.js
@@ -0,0 +1,73 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+
+    //compares entered passwored to hash password (runs on login)
+    validPassword(passwordTyped) {
+      return bcrypt.compareSync(passwordTyped, this.password) //returns a boolean o see if password type into the form matches the hashed password in the databaed
+    }
+
+    //remove the hashed password from the user object (before serializing)
+    toJSON() {
+      let userData = this.get() //.get built into get hte user we are working with
+      delete userData.password  //this removes the password so when we are passing around user info that the pw isnt a parto f that user object that gets passed between the backend and frontend..but does not remove it from the database
+      return userData
+    }
+  };
+  user.init({
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+//we're adding our hook, we are modifying the user before we save them to the database
+
+user.beforeCreate( (pendingUser, options) => { // callback function: pending user is a variable that represents the user about to be saved to the database; options is a parameter we dont end up using but put it there just so that yo know it exists
+  //if a user exists and if that user has a password
+  if (pendingUser && pendingUser.password) { //comes from the form up there
+    //hash the password with bcrypt
+    let hash = bcrypt.hashSync(pendingUser.password, 12) //HASHING FUNCTION takes  pending user password string and then set number of hash rounds (12)
+    //store the hash as the user's password
+    pendingUser.password = hash 
+  }
+})
+  return user;
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const db = require('../models')
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
 });
+
+//create a signup POST route
+router.post('/signup', (req, res) =>  {
+  //finding or creaing a user, given their name, password, and email
+  db.user.findOrCreate({
+    where: {email: req.body.email},
+    defaults: {
+      name: req.body.name,
+      password: req.body.password,
+    }
+  }).then(([user, created]) => {
+    //if created, this means success and we can redirect to home
+    if (created) {
+      console.log (`${user.name} was created!`)
+      res.redirect('/')
+    } else {
+      //if not created, the email already exists
+      console.log("Email already exists.")
+      res.redirect('/auth/login')
+    }
+  }).catch(error => { //"graceful error handling" catch and error, see what error is and send them to an error page
+    //if an error occurs lets see the error
+    console.log(`An error occured: ${error.message}`)
+    res.redirect('/auth/signup')
+  })
+})
+//you want to be taking note of the concepts behind a long hairy function like this.. what are the big decisions we are makaing inside of this signup funciton to make sure it is handling the users process of signining up in a way that makes sense and pushes them in the right places they need to be
+// - when it does well it redirects them to home
+// - when they already have an email they send them back to a logon
+// - if theres an error message thhen we send them back to the sign up page to try again
 
 router.get('/login', (req, res) => {
   res.render('auth/login');
