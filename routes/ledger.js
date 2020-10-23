f99916881889060a1151680bf85c4f36cbd887b2
smashflashtech <smash.flash.tech@gmail.com>
2020-10-23 16:07:33 -0700

    creates boilerplate with auth working and pseudocode

---
+++ routes/ledger.js
@@ -0,0 +1,15 @@
+//TO DO: REQUIRE STATEMENTS
+// - express
+// - express router
+// - method override
+
+
+//TO DO: GET /ledger - return a page with favorited plants
+//TO DO: - Get all records from the leafy_ledger database and render to view
+
+//TO DO: POST /ledger - receive name of plant, scientific name, picture URL, and API index integer and add it to the database (use hidden form data)
+//TO DO: - Get form data and add new record to the DB
+
+//TO DO: ADD A ROUTE FOR DELETING USING METHOD OVERRIDE ON THE POST ROUTE for deleting a plant form the database (the delete buttton is on the 'ledger.ejs')
+
+//TO DO: export the router
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const passport = require('./config/ppConfig')
 const flash = require('connect-flash')
 const isLoggedIn = require('./middleware/isLoggedIn')
 const app = express();
+//require axios - install axios too
 
+//~~~~~~~~~~~~~~~~~~~~MIDDLEWARE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
@@ -15,40 +17,44 @@ app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 
 app.use(session({
-  secret: process.env.SESSION_SECRET, //session_secret is a pizza variable name
+  secret: process.env.SESSION_SECRET,                                       //session_secret is a pizza variable name
   resave: false,
   saveUninitialized: true,
 }))
-//*********SESSION SHOULD ALWAYS BE ABOVE THESE LINES OF CODE below */
+//                                                                          //*********SESSION SHOULD ALWAYS BE ABOVE THESE LINES OF CODE below */
 app.use(flash())
 
-//*********SESSION SHOULD ALWAYS BE ABOVE THESE LINES OF CODE below */
+//                                                                          //*********SESSION SHOULD ALWAYS BE ABOVE THESE LINES OF CODE below */
 app.use(passport.initialize())
 app.use(passport.session())
 
-//next is a function that will just push a .. through to the next function variable - HAD A TYPPO SOMEWHERE
-// app.use(req, resp, next) => {
-//   //before every route, attach the flash messages and currentuser to res.locales - an object that stores
-//   res.locals.alerts=req.flash() //req.flash is added by the two middleware we place above
-//   res.locals.currentUser = req.user //this user property is added to each request by session and passport
-// })
 app.use((req, res, next) => {
-  // before every route, attach the flash messages and current user to res.locals
-  res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
-  next();
+// before every route, attach the flash messages and current user to res.locals
+  res.locals.alerts = req.flash();                                          //req.flash is added by the two middleware we place above
+  res.locals.currentUser = req.user;                                        //this user property is added to each request by session and passport
+  next();                                                                   //next is a function that will make sure the user is push through to the next route
 });
 
-app.get('/', (req, res) => {
-  res.render('index');
+//~~~~~~~~~~~~~~~~~~~~ROUTES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+app.get('/', (req, res) => {                                                //this is rendering the login page (views/index.ejs inside the template layout.ejs)
+  res.render('index');                                                      //layout.ejs determines whether or not a signup/login is displayed or lougout/profile is displayed
+});
+//PRIORITIZE THIS
+app.get('/main', isLoggedIn, (req, res) => {                                //this is rendering the profile page (views/profile.ejs)
+//TODO: use a request to call the API
+//  store desired API repsonse data in in a variable
+// res.render to the 'main' page with a context variable that contains API data - consider using .slice(startIndex, endIndex) to set API limit -- 1 million is a lot of plants to display
 });
 
-app.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile');
-});
+//TO DO: GET route by params to query the API for info on one plant and send to the details.ejs
 
+
+//~~~~~~~~~~~~~~~~~~~~ROUTES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 app.use('/auth', require('./routes/auth'));
+//TO DO: ADD IMPORT FOR NEW ROUTES
 
-var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
+
+var server = app.listen(process.env.PORT || 1031, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 1031}🎧`));
 
 module.exports = server;
