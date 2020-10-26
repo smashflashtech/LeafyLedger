a53a3be53b5201b22779bf8733c647a4534de959
smashflashtech <smash.flash.tech@gmail.com>
2020-10-25 17:32:55 -0700

    adds working POST route to find/create new plants and join with current user

--- routes/ledger.js
+++ routes/ledger.js
@@ -4,6 +4,9 @@ const db = require('../models')     // - DB
 var router = express.Router();      // - express router
 // - method override
 
+//~~~~~~~MiddleWARE~~~~~~~~~~~~~~~//
+router.use(express.urlencoded({ extended: false }));
+
 
 //TO DO: GET /ledger - return a page with favorited plants
 //TO DO: - Get all records from the leafy_ledger database and render to view
@@ -11,33 +14,35 @@ var router = express.Router();      // - express router
 //TO DO: POST /ledger - receive name of plant, scientific name, picture URL, and API index integer and add it to the database (use hidden form data)
 //TO DO: - Get form data and add new record to the DB
 router.post('/', (req, res) => {
-  console.log("This is: ", req.body.userId)
+  console.log("This is IT: ", req.body)
   db.user.findOrCreate({
-    where: {userId: req.body.userId}
+    where: { id: parseInt(req.body.userId) }
   }).then(function ([returnedUser, created]) {
-    console.log("This is 2nd: ", req.body.name)
-    console.log("User created?:", created)
-    //console.log("This is the data:", returnedUser)
+    console.log("THIS IS THE USER~~~~~~~~~", returnedUser.name, created)
     db.plant.findOrCreate({
-      where: { 
+      where: {
+        common_name: req.body.common_name,
+        scientific_name: req.body.scientific_name,
+        image_url: req.body.image_url,
         apiId: req.body.apiId,
-        name: req.body.name,
-        scientificName: req.body.scientificName,
-        lastWatered: "",
-        pictureUrl: req.body.pictureUrl,
+        lastWatered: req.body.lastWatered,
+        status: req.body.status,
       }
-    }).then(function (plant) {
-    returnedUser.addPlant(plant).then(function (userInfo) {
-    console.log(projectInfo)
-      })
+  }).then(function (newPlant) {
+    console.log("YOU HAVE ARRIVE!!!!!!!!!)")
+    console.log("NEW PLANT:", newPlant[0].id)
+    console.log("RETURNED USER:", returnedUser.name)
+    returnedUser.addPlant(newPlant[0].id).then(function(userInfo) {
+    console.log(newPlant[0].common_name, "WAS CREATED~~~~~~~~~~~**")
     })
   })
-    .then((user) => {
-      res.redirect('/')                                 //promises to redirect back to the home page
-    })
+})
+  .then((returnedUser) => {
+    res.redirect('/ledger')                                 //promises to redirect back to the ledger
+  })
     .catch((error) => {                                 //displays error if one occurs 
       //console.log(error)
-      res.status(400).render('main/404')
+      res.status(400).render('404')
     })
 })
 
