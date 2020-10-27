9d12a475179caf6bf3877ebb26ee92dad6f5e236
Caseykins <caseyjenkins92@gmail.com>
2020-10-27 10:26:43 -0600

    Working through delete function

--- routes/cemetery.js
+++ routes/cemetery.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const axios = require('axios')
 const db = require('../models')
 const isLoggedIn = require('../middleware/isLoggedIn')
+const methodOverride = require('method-override') // npm install method-override // refactor?
 
-
+router.use(methodOverride('_method'))
 
 router.get('/', isLoggedIn, function(req,res) {
     req.user.getPlants({
@@ -12,11 +13,30 @@ router.get('/', isLoggedIn, function(req,res) {
             status: 'dead',
         }
     }).then(function(deadPlants) {
-        // console.log('==========THIS IS MY BACKEND', deadPlants)
         res.render('cemetery', { plants: deadPlants } )
     })
 })
 
+router.delete('/:id', isLoggedIn, function(req, res) {
+    console.log(req.params) // plant id
+    console.log(req.user)
+    req.user.getPlants({
+        where: {
+            id: // not sure what goes here (NOT req.params.id),
+            // we want to delete from join table NOT plant table
+        }
+    }).then(function(plantHell){
+        db.plantsUsers.destroy({
+            // Not sure how to proceed from here
+        })
+    })
+    // db.plant.destroy({
+    //   where: { plant: plant }
+    // }).then(function() {
+    //   // do something when done deleting
+    //   res.redirect('/cemetery')
+    // });
+})
 
 
 module.exports = router;
