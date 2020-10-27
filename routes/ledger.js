5391ca027c79c2b807db0823551071959a61b279
smashflashtech <smash.flash.tech@gmail.com>
2020-10-27 15:57:36 -0700

    updates save to ledger to add duplicate plants, creates route for get edit page, creates put route for updating lastWatered

--- routes/ledger.js
+++ routes/ledger.js
@@ -18,7 +18,7 @@ router.get("/", isLoggedIn, function(req, res) {
       }
     }
     ).then(function(alivePlants){
-    console.log(alivePlants)
+    //console.log(alivePlants)
     res.render("ledger", { plants: alivePlants})
   }) 
 })
@@ -31,21 +31,21 @@ router.post('/', (req, res) => {
     where: { id: parseInt(req.body.userId) }
   }).then(function ([returnedUser, created]) {
     // console.log("THIS IS THE USER~~~~~~~~~", returnedUser.name, created)
-    db.plant.findOrCreate({
-      where: {
+    db.plant.create(
+      {
         common_name: req.body.common_name,
         scientific_name: req.body.scientific_name,
         image_url: req.body.image_url,
         apiId: req.body.apiId,
         lastWatered: req.body.lastWatered,
         status: req.body.status,
-      }
-  }).then(function (newPlant) {
+      }                                   ///ATTENTION: this is working we are happy with this// add to the database
+  ).then(function (newPlant) {
     // console.log("YOU HAVE ARRIVE!!!!!!!!!)")
-    // console.log("NEW PLANT:", newPlant[0].id)
+    //console.log("NEW PLANT:", newPlant.dataValues)
     // console.log("RETURNED USER:", returnedUser.name)
-    returnedUser.addPlant(newPlant[0].id).then(function(userInfo) {
-    // console.log(newPlant[0].common_name, "WAS CREATED~~~~~~~~~~~**")
+    returnedUser.addPlant(newPlant.dataValues.id).then(function(userInfo) {
+    //console.log(newPlant.dataValues.common_name, "WAS CREATED~~~~~~~~~~~** and added to", returnedUser)
     })
   })
 })
@@ -74,12 +74,31 @@ router.delete('/:id', isLoggedIn, function(req, res) {
 })
 
 
+//ROUTE for GET page for EDITTING specific plant water
+router.get('/edit/:id', function (req, res) {
+  //console.log("~~~~~~~~~~~~~NINJA UNITE", req.params)
+  db.plant.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(foundPlant){
+    res.render('edit', {editPlant: foundPlant})
+    })
+})
 
-
-
-
-
-
+//UPDATE by PUT the new data for lastWatered into the database
+router.put('/:id', function(req, res) {
+  //console.log('THIS IS RECK DAT BODY~~~~~~~~~~~~~~~~~~~', req.body.lastWatered) //GOT IT!
+  //console.log('THIS IS RECK DAT PARAMs~~~~~~~~~~~~~~~~~~~', req.params)//plant id
+  //we need to only edit plant for the specific user
+  //it doesnt matter who the user is because now plant IDs are Unique to their user--because now we have a many to many relationship
+  db.plant.update(
+    { lastWatered: req.body.lastWatered },
+    { where: {id: req.params.id }}
+  ).then(
+    res.redirect('/ledger')
+    )
+})
 
 
 module.exports = router;
